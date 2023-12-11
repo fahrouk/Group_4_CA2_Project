8dbf2581cf0e2f3f4f90961aae6fc2e10d55a659
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-11 16:43:09 +0000

    Update app.py
    karthik commit app.py 3

--- app.py
+++ app.py
@@ -10,3 +10,26 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = "CA2_Project"
 Session(app)
+db = SQL ( "sqlite:///data.db" )
+@app.route("/")
+def index():
+    books = db.execute("select * FROM books")
+    booksLen = len(books)
+    shoppingCart = []
+    shopLen = len(shoppingCart)
+    totItems= 0
+    total=0
+    display=0
+    if 'user' in session:
+        shoppingCart = db.execute("select image, SUM(qty), SUM(subTotal), price, id FROM cart")
+        shopLen = len(shoppingCart)
+        for i in range(shopLen):
+            total += shoppingCart[i]["SUM(subTotal)"]
+            totItems += shoppingCart[i]["SUM(qty)"]
+        books = db.execute("SELECT * FROM books")
+        booksLen = len(books)
+        return render_template ("index.html", shoppingCart=shoppingCart, books=books, shopLen=shopLen, booksLen=booksLen, total=total, totItems=totItems, display=display, session=session )
+    return render_template ("index.html", books=books, shoppingCart=shoppingCart, booksLen=booksLen, shopLen=shopLen, total=total, totItems=totItems, display=display)
+
+@app.route("/login/", methods=["GET"])
+def login():
