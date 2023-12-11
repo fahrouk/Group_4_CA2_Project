731f9bec1bc755c05adf4e09e2e9fe4b5d1d2b7b
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-11 16:27:50 +0000

    Update app.py
    karthik commit app.py 2

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ from flask_session import Session
 from flask import Flask, render_template,session, redirect,request
 from datetime import datetime
 import locale
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "CA2_Project"
+Session(app)
