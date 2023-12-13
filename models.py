0bb6a35a3ffd187923a8366e06d2d51f8bc30e26
karthikeyankkn7 <152183950+karthikeyankkn7@users.noreply.github.com>
2023-12-13 10:58:37 +0000

    Updated class cart models.py

--- models.py
+++ models.py
@@ -1,2 +1,9 @@
 from app import db
 
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.Text)
+    qty = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    subTotal = db.Column(db.Float)
+
